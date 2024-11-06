f4e7a7bff3220825abb8a3cd3c028083aea59a06
Odilbek Arziev <odarziev@mail.ru>
2024-11-06 12:05:45 +0300

    delete todo added

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,10 +1,26 @@
 import React from "react";
 
+
 export default function Todo({ todos, setTodos, todo }) {
+  function completeTodo(todo) {
+    let newTodos = todos.map((task) => {
+      if (task.id === todo.id) {
+        return {...task, completed: !todo.completed}
+      }
+      return task
+    })
+    setTodos(newTodos)    
+  }
+
   return (
     <div className="todo">
       <div className={(todo.completed) ? 'completed' : ''}>
-        <input type="checkbox" checked={todo.completed} className="checkbox"/>
+        <input
+          type="checkbox"
+          checked={todo.completed}
+          className="checkbox"
+          onChange={() => completeTodo(todo)}
+        />
         <label htmlFor={`check-${todo.id}`}>{todo.title}</label>
       </div>
     </div>
