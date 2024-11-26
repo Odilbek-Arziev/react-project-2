4074488b4e7419c990c2fbb6095b9f1df63e61a4
Odilbek Arziev <odarziev@mail.ru>
2024-11-26 11:39:50 +0300

    fetch & axios

--- src/index.js
+++ src/index.js
@@ -1,9 +1,53 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Main from "./pages/Main";
-import './static/style.css'
+import "./static/style.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<div />);
+root.render(<Main />);
 
-// JSON - javascript object notation
+
+// синтаксис fetch-запроса
+
+// fetch(
+// запрос на сервер
+// преобразование json в объект js
+// вывод информации
+// обработка ошибок
+// )
+
+// стандартный get-запрос
+
+// let request = fetch("https://jsonplaceholder.typicode.com/todos?_limit=10");
+
+// request
+//   .then((response) => response.json())
+//   .then((data) => console.log(data))
+//   .catch((error) => console.error(error));
+
+// get-запрос с конкретным id
+// let request = fetch("https://jsonplaceholder.typicode.com/todos/1");
+
+// request
+//   .then((response) => response.json())
+//   .then((data) => console.log(data))
+//   .catch((error) => console.error(error));
+
+// post-запрос
+// let request = fetch("https://jsonplaceholder.typicode.com/todos", {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+//   body: JSON.stringify({
+//     title: "New todo",
+//     completed: false,
+//     userId: 1,
+//   }),
+// });
+
+// request
+//   .then((response) => response.json())
+//   .then((data) => console.log(data))
+//   .catch((error) => console.error(error));
+
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,13 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Form from "../components/Form";
 import Todos from "../components/Todos";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import { todosArray } from '../utils/todos'
+import { todosArray } from "../utils/todos";
+import axios from "axios";
 
 export default function Main() {
   const [todos, setTodos] = useState(todosArray);
   const [editTodo, setEditTodo] = useState(null);
+
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/todos?_limit=10")
+      .then((response) => setPosts(response.data))
+      .catch((error) => console.error(error));
+  }, []);
+
+  console.log(posts);
 
   return (
     <div className="container">
